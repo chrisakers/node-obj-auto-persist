f7f63d6e00b9d596154d5eff87001d7456a673e8
Chris Akers <chris.akers@gmail.com>
2015-06-08 22:50:52 -0400

    add default object parameter/comments

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var fs = require('fs');
 
-function persistObj(fileName) {
+/**
+ * Returns the persisted object and observes it for future persistence
+ * @param  {string} fileName   the absolute path and name of the file used for persisting the object
+ * @param  {object} [original] the optional default object to return if the file does not already exist
+ * @return {object}            the object loaded from the filesystem which is observed for future changes
+ */
+function persistObj(fileName, original) {
     var obj;
 
     try {
         //TODO: Add configurable serialization/deserialization?
         obj = require(fileName);
     } catch (err) {
-        obj = {};
+        obj = original || {};
     }
 
+    //TODO: observe only certain types of changes?
     Object.observe(obj, function () {
         //TODO: Add throttling?
         fs.writeFile(fileName, JSON.stringify(obj, null, 4));
